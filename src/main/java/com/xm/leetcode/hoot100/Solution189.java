2a0e1e48255939321eee5c0a25f3893a15161a19
admin <xmwencom@qq.com>
2026-07-11 18:27:29 +0800

    189. 轮转数组

---
+++ src/main/java/com/xm/leetcode/hoot100/Solution189.java
@@ -0,0 +1,19 @@
+package com.xm.leetcode.hoot100;
+
+class Solution189 {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int temp = nums[l];
+            nums[l++] = nums[r];
+            nums[r--] = temp;
+        }
+    }
+}
