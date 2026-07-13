ac589362b0c2424651b9099ce02dd6c38769a488
admin <xmwencom@qq.com>
2026-07-13 16:31:16 +0800

    141. 环形链表

---
+++ src/main/java/com/xm/leetcode/hoot100/Solution141.java
@@ -0,0 +1,29 @@
+package com.xm.leetcode.hoot100;
+
+import com.xm.leetcode.common.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
