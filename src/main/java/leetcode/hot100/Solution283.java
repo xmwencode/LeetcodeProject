3e7dce541f7a2821e4390a19a627cfe1dd524dd2
xm.wen <xm.wen@tec-do.com>
2026-07-31 17:41:02 +0800

    283. 移动零

---
+++ src/main/java/leetcode/hot100/Solution283.java
@@ -0,0 +1,15 @@
+package leetcode.hot100;
+
+class Solution283 {
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[pos++] = nums[i];
+            }
+        }
+        while (pos < nums.length) {
+            nums[pos++] = 0;
+        }
+    }
+}
