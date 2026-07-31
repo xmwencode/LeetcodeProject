f7ee5960b6070961eb92e65ac4109b983ba21d93
xm.wen <xm.wen@tec-do.com>
2026-07-31 17:43:46 +0800

    11. 盛最多水的容器

---
+++ src/main/java/leetcode/hot100/Solution11.java
@@ -0,0 +1,17 @@
+package leetcode.hot100;
+
+class Solution11 {
+    public int maxArea(int[] height) {
+        int ans = 0;
+        int l = 0, r = height.length - 1;
+        while (l < r) {
+            ans = Math.max(ans, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return ans;
+    }
+}
