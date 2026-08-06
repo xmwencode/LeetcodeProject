fd23deaad76b23677dbc2e8eb4bd91610f521b30
xm.wen <xm.wen@tec-do.com>
2026-08-06 17:29:03 +0800

    48. 旋转图像

---
+++ src/main/java/leetcode/hot100/Solution48.java
@@ -0,0 +1,26 @@
+package leetcode.hot100;
+
+class Solution48 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 沿主对角线翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix, i, j, j, i);
+            }
+        }
+        // 左右翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix, i, j, i, n - j - 1);
+            }
+        }
+
+    }
+
+    private static void swap(int[][] matrix, int i1, int j1, int i2, int j2) {
+        int tmp = matrix[i1][j1];
+        matrix[i1][j1] = matrix[i2][j2];
+        matrix[i2][j2] = tmp;
+    }
+}
