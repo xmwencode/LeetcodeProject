a8897b1c077f38e3e1030958dc944b4d3af390c3
xm.wen <xm.wen@tec-do.com>
2026-08-06 18:05:54 +0800

    234. 回文链表

---
+++ src/main/java/leetcode/hot100/Solution234.java
@@ -0,0 +1,43 @@
+package leetcode.hot100;
+
+import leetcode.utils.ListNode;
+
+class Solution234 {
+    public boolean isPalindrome(ListNode head) {
+        // 寻找链表的中间节点
+        ListNode mid = middleNode(head);
+        // 翻转链表
+        ListNode rev = reverseList(mid);
+        while (rev != null) {
+            if (head.val != rev.val) {
+                return false;
+            }
+            head = head.next;
+            rev = rev.next;
+        }
+        return true;
+    }
+
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
