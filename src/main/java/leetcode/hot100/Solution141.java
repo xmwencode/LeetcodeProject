84cb894059175b2b09b46279a4681c60ff05e579
xm.wen <xm.wen@tec-do.com>
2026-08-06 18:08:10 +0800

    141. 环形链表

---
+++ src/main/java/leetcode/hot100/Solution141.java
@@ -0,0 +1,16 @@
+package leetcode.hot100;
+
+import leetcode.utils.ListNode;
+
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
