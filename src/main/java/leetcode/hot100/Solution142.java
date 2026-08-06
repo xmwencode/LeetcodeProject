8a85cafea89e49ee2330d585c89b45e33e891189
xm.wen <xm.wen@tec-do.com>
2026-08-06 18:12:32 +0800

    142. 环形链表 II

---
+++ src/main/java/leetcode/hot100/Solution142.java
@@ -0,0 +1,29 @@
+package leetcode.hot100;
+
+import leetcode.utils.ListNode;
+
+public class Solution142 {
+    public ListNode detectCycle(ListNode head) {
+        // 找到在环中的交点
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+        if (!hasCycle) {
+            return null;
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
